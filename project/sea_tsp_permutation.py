189ca5537182b49ccb4d9f32c469c436595a3595
goncalonsp <goncalonsp@gmail.com>
2017-05-09 01:24:27 +0100

    Implemented alternative representation using permutations for the TSP problem. Also added a new configuration for the permutation representation.

---
+++ project/sea_tsp_permutation.py
@@ -0,0 +1,224 @@
+"""
+sea_bin.py
+A very simple EA for float representation.
+Ernesto Costa, March 2015 & February 2016
+Adjusted by Sebastian Rehfeldt
+"""
+
+
+from random import random,randint, sample, gauss, shuffle
+from operator import itemgetter
+
+# Simple Evolutionary Algorithm     
+def sea(numb_generations,size_pop, size_cromo, prob_mut, sigma, prob_cross,sel_parents,recombination,mutation,sel_survivors, fitness_func):
+    # initialize population: indiv = (cromo,fit)
+    population = gera_pop(size_pop,size_cromo)
+    # evaluate population
+    population = [(indiv[0], fitness_func(indiv[0])) for indiv in population]
+    for i in range(numb_generations):
+        # parents selection
+        mate_pool = sel_parents(population)
+    # Variation
+    # ------ Crossover
+        parents = []
+        for i in  range(0,size_pop-1,2):
+            indiv_1= mate_pool[i]
+            indiv_2 = mate_pool[i+1]
+            children = recombination(indiv_1,indiv_2, prob_cross)
+            parents.extend(children) 
+        # ------ Mutation
+        descendants = []
+        for cromo,fit in parents:
+            new_indiv = mutation(cromo,prob_mut)
+            descendants.append((new_indiv,fitness_func(new_indiv)))
+        # New population
+        population = sel_survivors(population,descendants)
+        # Evaluate the new population
+        population = [(indiv[0], fitness_func(indiv[0])) for indiv in population]   
+
+    """
+    return: the best individual found
+            the population of the final generation
+    """
+    return best_pop(population), population
+
+def sea_for_plot(numb_generations,size_pop, size_cromo, prob_mut, sigma, prob_cross,sel_parents,recombination,mutation,sel_survivors, fitness_func):
+    # initialize population: indiv = (cromo,fit)
+    population = gera_pop(size_pop,size_cromo)
+    # evaluate population
+    population = [(indiv[0], fitness_func(indiv[0])) for indiv in population]
+
+    stat = [best_pop(population)[1]]
+    stat_aver = [average_pop(population)]
+
+    for i in range(numb_generations):
+        # parents selection
+        mate_pool = sel_parents(population)
+	# Variation
+	# ------ Crossover
+        parents = []
+        for i in  range(0,size_pop-1,2):
+            indiv_1= mate_pool[i]
+            indiv_2 = mate_pool[i+1]
+            children = recombination(indiv_1,indiv_2, prob_cross)
+            parents.extend(children) 
+        # ------ Mutation
+        descendants = []
+        for cromo,fit in parents:
+            new_indiv = mutation(cromo,prob_mut)
+            descendants.append((new_indiv,fitness_func(new_indiv)))
+        # New population
+        population = sel_survivors(population,descendants)
+        # Evaluate the new population
+        population = [(indiv[0], fitness_func(indiv[0])) for indiv in population]   
+
+        stat.append(best_pop(population)[1])
+        stat_aver.append(average_pop(population))
+
+    """
+    return: the best individual found
+            the population of the final generation
+            the best individual from each generation
+            the average individual from each generation
+    """
+    return best_pop(population), population, stat, stat_aver
+
+def run(numb_runs,numb_generations,size_pop, domain, prob_mut, sigma, prob_cross,sel_parents,recombination,mutation,sel_survivors, fitness_func):
+    statistics = []
+    bestTours = []
+    for i in range(numb_runs):
+        best, population, stat_best, stat_aver = sea_for_plot(numb_generations,size_pop, domain, prob_mut, sigma, prob_cross,sel_parents,recombination,mutation,sel_survivors, fitness_func)
+        bestTours.append(best)
+        statistics.append(stat_best)
+    stat_gener = list(zip(*statistics))
+    best = [min(g_i) for g_i in stat_gener] # minimization
+    aver_gener =  [sum(g_i)/len(g_i) for g_i in stat_gener]
+    return best,aver_gener,bestTours
+    
+
+# Initialize population
+def gera_pop(size_pop,size_cromo):
+    return [(gera_indiv(size_cromo),0) for i in range(size_pop)]
+
+def gera_indiv(size_cromo):
+    # random permutation initialization
+    indiv = list(range(1,size_cromo+1))
+    shuffle(indiv)
+    return indiv
+
+# Variation operators: Permutation Mutation    
+def muta_permutation(indiv,prob_muta):
+    # Mutation by swapping genes' places
+    cromo = indiv[:]
+    for i in range(len(indiv)):
+        if random() < prob_muta:
+            j = randint(0,len(cromo)-1)
+            tmp = cromo[i]
+            cromo[i] = cromo[j]
+            cromo[j] = tmp
+    return cromo
+
+# Variation Operators :Crossover
+# Credits to:
+# http://www.rubicite.com/Tutorials/GeneticAlgorithms/CrossoverOperators/CycleCrossoverOperator.aspx
+def cycle_crossover(indiv_1, indiv_2, prob_cross):
+    value = random()
+    if value < prob_cross:
+        cromo1 = indiv_1[0]
+        cromo2 = indiv_2[0]
+
+        # To keep track of 'seen' indexes
+        marked = [False] * len(cromo1)
+
+        # Identify cycles
+        cycles = []
+        for idx1 in range(0, len(cromo1)):
+            cycle = []
+
+            if not marked[idx1]:
+                
+                # Get the position of the value in indiv 1 in indiv 2
+                idx2 = cromo2.index(cromo1[idx1])
+                # Add a part of the cycle
+                # (the index, indiv 2 index value, indiv 1 index value)
+                cycle.append((idx2, cromo2[idx2], cromo1[idx2]))
+                # mark the index as seen
+                marked[idx2] = True
+
+                # until we loop completely
+                while idx2 != idx1:
+                    # repeat as above using the index of the indiv 1 value in indiv 2
+                    idx2 = cromo2.index(cromo1[idx2])
+                    cycle.append((idx2, cromo2[idx2], cromo1[idx2]))
+                    marked[idx2] = True
+
+                # add the found cycle
+                cycles.append(cycle)
+
+        # Copy the material from the cycles to the offspring
+        child1 = [None] * len(cromo1)
+        child2 = [None] * len(cromo1)
+        # Alternate between each cycle, first for child 1, second for 2
+        alt = True
+        for cycle in cycles:
+            for part in cycle:
+                if alt:
+                    child1[part[0]] = part[2]
+                    child2[part[0]] = part[1]
+                else:
+                    child1[part[0]] = part[1]
+                    child2[part[0]] = part[2]
+            alt ^= True
+
+        return ((child1,0),(child2,0))
+    else:
+        return (indiv_1, indiv_2)
+
+# Parents Selection: tournament
+def tour_sel(t_size):
+    def tournament(pop):
+        size_pop= len(pop)
+        mate_pool = []
+        for i in range(size_pop):
+            winner = one_tour(pop,t_size)
+            mate_pool.append(winner)
+        return mate_pool
+    return tournament
+
+def one_tour(population,size):
+    """Maximization Problem. Deterministic"""
+    pool = sample(population, size)
+    pool.sort(key=itemgetter(1), reverse=False)
+    return pool[0]
+
+
+# Survivals Selection: elitism
+def sel_survivors_elite(elite):
+    def elitism(parents,offspring):
+        size = len(parents)
+        comp_elite = int(size* elite)
+        offspring.sort(key=itemgetter(1), reverse=False)
+        parents.sort(key=itemgetter(1), reverse=False)
+        new_population = parents[:comp_elite] + offspring[:size - comp_elite]
+        return new_population
+    return elitism
+
+
+# Auxiliary
+    
+def best_pop(population):
+    population.sort(key=itemgetter(1),reverse=False)
+    return population[0]
+
+def average_pop(population):
+    return sum([fit for cromo,fit in population])/len(population)
+
+if __name__ == '__main__':
+    # run to test cycle crossover
+    p1 = ([1, 2, 3, 4, 5, 6, 7, 8, 9],0)
+    p2 = ([9, 3, 7, 8, 2, 6, 5, 1, 4],0)
+    print("Parent 1: {}".format(p1))
+    print("Parent 2: {}".format(p2))
+    (c1, c2) = cycle_crossover(p1, p2, 1)
+    print("Child  1: {}".format(c1))
+    print("Child  2: {}".format(c2))
--- project/tsp.py
+++ project/tsp.py
@@ -4,18 +4,20 @@ Sebastian Rehfeldt, April 2017
 Based on Ernesto Costa, February 2017
 """
 
-from sea_tsp import *
+import sea_tsp_randkey
+import sea_tsp_permutation
 from utils import *
 from copy import deepcopy
 from config import get_config
 
-# fitness
-def fitness(distmat):
+# fitness for rand key representation
+def fitness_randkey(distmat):
     def fitness_(indiv):
-        return evaluate(phenotype(indiv),distmat)
+        return evaluate(phenotype_from_randkey(indiv),distmat)
     return fitness_
 
-def phenotype(genotype):
+# for rand key representation
+def phenotype_from_randkey(genotype):
     """ Return the phenotype which is the tour."""
     # phenotype is a permutation of integers higher than 1 where each integer represents a city
     # the values are higher than 0 as the first city is fixed to the first city with index 0
@@ -39,7 +41,23 @@ def phenotype(genotype):
         genoTemp[index] = 2 #this is necessary in the case that random values are duplicated
     return pheno
 
+# fitness for permutation representation
+def fitness_permutation(distmat):
+    def fitness_(indiv):
+        return evaluate(phenotype_from_permutation(indiv),distmat)
+    return fitness_
 
+# for permutation representation
+def phenotype_from_permutation(genotype):
+    """ Return the phenotype which is the tour."""
+    # phenotype is a permutation of integers higher than 1 where each integer represents a city
+    # the values are higher than 0 as the first city is fixed to the first city with index 0
+    #   for example [0, 1, 4, 6, 2, 3, 5, 7, 9, 8] 
+    #
+    # this function interprets a permutation genotype which is a direct representation
+    return list(genotype)
+
+# Phenotype evaluation, phenotype is a list of integers, ex: [0, 1, 4, 6, 2, 3, 5, 7, 9, 8]
 def evaluate(tour,distmat):
     number_of_cities = len(tour)
     distance = distmat[0,tour[0]] #distance from fixed starting point to first city of tour
@@ -51,6 +69,7 @@ def evaluate(tour,distmat):
     distance += distmat[0,tour[number_of_cities-1]]
 
     return distance
+
 
 def select_best_k_tours(population,k):
     population.sort(key=itemgetter(1),reverse=False)
@@ -72,7 +91,6 @@ def select_best_k_tours(population,k):
 
 def getTours(distmat, configs, ntours):
 
-    my_fitness = fitness(distmat)
     size_cromo = distmat.shape[0]-1 # as the starting and ending point is fixed
     
     #geno = [0.4,0.7,0.4]
@@ -96,22 +114,92 @@ def getTours(distmat, configs, ntours):
     tsp_development = get_config(configs, ['development'], False)
     tsp_plot_generations = get_config(configs, ['plot_generations'], True)
 
+    # Define the representation to be used
+    representation = get_config(configs, ['representation'], "random key")
+    if representation == "random key":
+
+        my_fitness = fitness_randkey(distmat)
+        sea = sea_tsp_randkey.sea
+        sea_for_plot = sea_tsp_randkey.sea_for_plot
+        run = sea_tsp_randkey.run
+        crossover = sea_tsp_randkey.two_points_cross
+        mutation = sea_tsp_randkey.muta_float_gaussian
+        tour_selection = sea_tsp_randkey.tour_sel(tour_size)
+        sel_survivors = sea_tsp_randkey.sel_survivors_elite(elite_size)
+
+    elif representation == "permutation":
+        
+        my_fitness = fitness_permutation(distmat)
+        sea = sea_tsp_permutation.sea
+        sea_for_plot = sea_tsp_permutation.sea_for_plot
+        run = sea_tsp_permutation.run
+        crossover = sea_tsp_permutation.cycle_crossover
+        mutation = sea_tsp_permutation.muta_permutation
+        tour_selection = sea_tsp_permutation.tour_sel(tour_size)
+        sel_survivors = sea_tsp_permutation.sel_survivors_elite(elite_size)
+
+    else:
+        raise LookupError('Unknown representation \'{}\' chosen in configuration!'.format(representation))
+
+
     bestTours = []
 
     if(tsp_development):
         if(tsp_plot_generations):
-            best, population, stat, stat_average = sea_for_plot(generations,population_size, size_cromo, prob_muta, sigma, prob_cross,tour_sel(tour_size),two_points_cross,muta_float_gaussian,sel_survivors_elite(elite_size), my_fitness)
+            best, population, stat, stat_average = sea_for_plot(
+                                                        generations,
+                                                        population_size, 
+                                                        size_cromo, 
+                                                        prob_muta, 
+                                                        sigma, 
+                                                        prob_cross,
+                                                        tour_selection,
+                                                        crossover,
+                                                        mutation,
+                                                        sel_survivors, 
+                                                        my_fitness)
+            
             display_stat_1(stat,stat_average)
             bestTours = select_best_k_tours(population, ntours)
         else:
-            best, population = sea(generations,population_size, size_cromo, prob_muta, sigma, prob_cross,tour_sel(tour_size),two_points_cross,muta_float_gaussian,sel_survivors_elite(elite_size), my_fitness)
+            best, population = sea(
+                                generations,
+                                population_size, 
+                                size_cromo, 
+                                prob_muta, 
+                                sigma, 
+                                prob_cross,
+                                tour_selection,
+                                crossover,
+                                mutation,
+                                sel_survivors, 
+                                my_fitness)
+
             bestTours = select_best_k_tours(population, ntours)
 
     else:
-        best, best_average, tours = run(runs, generations,population_size, size_cromo, prob_muta, sigma, prob_cross,tour_sel(tour_size),two_points_cross,muta_float_gaussian,sel_survivors_elite(elite_size), my_fitness)
-        display_stat_n(best,best_average)
+        best, best_average, tours = run(
+                                        runs, 
+                                        generations,
+                                        population_size, 
+                                        size_cromo, 
+                                        prob_muta, 
+                                        sigma, 
+                                        prob_cross,
+                                        tour_selection,
+                                        crossover,
+                                        mutation,
+                                        sel_survivors, 
+                                        my_fitness)
+
+        if(tsp_plot_generations):
+            display_stat_n(best,best_average)
+            
         #append first best tour to bestTours to allow rest of program to run
-        bestTours.append((phenotype(tours[0][0]),tours[0][1]))
+        if representation == "random key":
+            bestTours.append((phenotype_from_randkey(tours[0][0]),tours[0][1]))
+        elif representation == "permutation":
+            bestTours.append((phenotype_from_permutation(tours[0][0]),tours[0][1]))
     
     return bestTours
     
